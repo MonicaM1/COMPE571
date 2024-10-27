b0c18dde050a536f1794b6cb9e4b59d7a2642752
MonicaM1 <120605003+MonicaM1@users.noreply.github.com>
2024-10-26 17:58:28 -0700

    Update SJF.c

--- PA2/SJF.c
+++ PA2/SJF.c
@@ -101,29 +101,25 @@ int main(int argc, char const *argv[]) {
 		to be implemented.
 	************************************************************************************************/
 
-	struct timespec arrival_time, start_time[4], end_time[4];
+	struct timespec arrival_time, end_time[4];
 	clock_gettime(CLOCK_MONOTONIC, &arrival_time);
 
 	// Running Workload 4 (shortest)
-	clock_gettime(CLOCK_MONOTONIC, &start_time[3]);
 	kill(pid4, SIGCONT);
 	waitpid(pid4, NULL, 0);
 	clock_gettime(CLOCK_MONOTONIC, &end_time[3]);
 
 	// Running Workload 3
-	clock_gettime(CLOCK_MONOTONIC, &start_time[2]);
 	kill(pid3, SIGCONT);
 	waitpid(pid3, NULL, 0);
 	clock_gettime(CLOCK_MONOTONIC, &end_time[2]);
 
 	// Running Workload 2
-	clock_gettime(CLOCK_MONOTONIC, &start_time[1]);
 	kill(pid2, SIGCONT);
 	waitpid(pid2, NULL, 0);
 	clock_gettime(CLOCK_MONOTONIC, &end_time[1]);
 
 	// Running Workload 1 (longest)
-	clock_gettime(CLOCK_MONOTONIC, &start_time[0]);
 	kill(pid1, SIGCONT);
 	waitpid(pid1, NULL, 0);
 	clock_gettime(CLOCK_MONOTONIC, &end_time[0]);
@@ -132,6 +128,7 @@ int main(int argc, char const *argv[]) {
 		- Scheduling code ends here
 	************************************************************************************************/
 	float response_times[4];
+	printf("Shortest Job First:\n");
 	for (int i = 0; i < 4; i++)
 	{
 		response_times[i] = (((end_time[i].tv_sec - arrival_time.tv_sec) * 1e9) + (end_time[i].tv_nsec - arrival_time.tv_nsec)) * 1e-9;
