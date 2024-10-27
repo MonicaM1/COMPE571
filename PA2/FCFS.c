438926bfc8cbd7733f78fc444713abb854bea9d3
MonicaM1 <120605003+MonicaM1@users.noreply.github.com>
2024-10-26 17:44:15 -0700

    Create FCFS.c

---
+++ PA2/FCFS.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <stdint.h>
+
+/************************************************************************************************
+		These DEFINE statements represent the workload size of each task and
+		the time quantum values for Round Robin scheduling for each task.
+*************************************************************************************************/
+
+#define WORKLOAD1 100000
+#define WORKLOAD2 50000
+#define WORKLOAD3 25000
+#define WORKLOAD4 10000
+
+#define QUANTUM1 1000
+#define QUANTUM2 1000
+#define QUANTUM3 1000
+#define QUANTUM4 1000
+
+/************************************************************************************************
+					DO NOT CHANGE THE FUNCTION IMPLEMENTATION
+*************************************************************************************************/
+void myfunction(int param)
+{
+
+	int i = 2;
+	int j, k;
+
+	while (i < param)
+	{
+		k = i;
+		for (j = 2; j <= k; j++)
+		{
+			if (k % j == 0)
+			{
+				k = k / j;
+				j--;
+				if (k == 1)
+				{
+					break;
+				}
+			}
+		}
+		i++;
+	}
+}
+/************************************************************************************************/
+
+int main(int argc, char const *argv[])
+{
+	pid_t pid1, pid2, pid3, pid4;
+	int running1, running2, running3, running4;
+
+	pid1 = fork();
+
+	if (pid1 == 0)
+	{
+
+		myfunction(WORKLOAD1);
+
+		exit(0);
+	}
+	kill(pid1, SIGSTOP);
+
+	pid2 = fork();
+
+	if (pid2 == 0)
+	{
+
+		myfunction(WORKLOAD2);
+
+		exit(0);
+	}
+	kill(pid2, SIGSTOP);
+
+	pid3 = fork();
+
+	if (pid3 == 0)
+	{
+
+		myfunction(WORKLOAD3);
+
+		exit(0);
+	}
+	kill(pid3, SIGSTOP);
+
+	pid4 = fork();
+
+	if (pid4 == 0)
+	{
+
+		myfunction(WORKLOAD4);
+
+		exit(0);
+	}
+	kill(pid4, SIGSTOP);
+
+	/************************************************************************************************
+		At this point, all  newly-created child processes are stopped, and ready for scheduling.
+	*************************************************************************************************/
+
+	/************************************************************************************************
+		- Scheduling code starts here
+	************************************************************************************************/
+
+	struct timespec arrival_time, end_time[4];
+	clock_gettime(CLOCK_MONOTONIC, &arrival_time);
+	
+	kill(pid1, SIGCONT);
+	waitpid(pid1, NULL, 0);
+	clock_gettime(CLOCK_MONOTONIC, &end_time[0]);
+
+	kill(pid2, SIGCONT);
+	waitpid(pid2, NULL, 0);
+	clock_gettime(CLOCK_MONOTONIC, &end_time[1]);
+
+	kill(pid3, SIGCONT);
+	waitpid(pid3, NULL, 0);
+	clock_gettime(CLOCK_MONOTONIC, &end_time[2]);
+
+	kill(pid4, SIGCONT);
+	waitpid(pid4, NULL, 0);
+	clock_gettime(CLOCK_MONOTONIC, &end_time[3]);
+	
+	float response_times[4];
+	for (int i = 0; i < 4; i++)
+	{
+		response_times[i] = (((end_time[i].tv_sec - arrival_time.tv_sec) * 1e9) + (end_time[i].tv_nsec - arrival_time.tv_nsec)) * 1e-9;
+		printf("Task %d Response Time: %.8fs\n", i, response_times[i]);
+	}
+
+	/************************************************************************************************
+		- Scheduling code ends here
+	************************************************************************************************/
+
+	return 0;
+}
